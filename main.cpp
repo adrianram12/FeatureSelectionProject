f5c01e5499f953c900c50dfb2761012f131dc850
adrianram12 <adrianramos2121@gmail.com>
2022-12-06 11:14:57 -0800

    Began implementing the search portion of program

--- main.cpp
+++ main.cpp
@@ -4,8 +4,29 @@
 #include <vector>
 #include <sstream>
 #include <iomanip>
-
 using namespace std;
+
+void featureSearch(vector<vector<float> > data){
+
+    vector<int> currentSetOfFeatures;
+    float bestAccuracySoFar;
+    float accuracy;
+
+    for(int i = 0; i < data.size(); i++){
+
+        cout << "On the " << i + 1 << "th level of the search tree" << endl;
+
+        vector<int> featureToAddAtThisLevel;
+        bestAccuracySoFar = 0;
+
+
+        for(int j = 0; j < data.at(i).size() - 1; j++){
+
+            cout << "--Considering adding the " << j + 1 << " feature" << endl;
+            accuracy = leaveOneOutCrossValidation(data, currentSetOfFeatures, j + 1);
+        }
+    }
+}
 
 int main(){
 
@@ -50,7 +71,10 @@ int main(){
     //     cout << endl;
     // }
 
+    //cout << fixed << setprecision(7) << dataTable.at(0).at(1) << " times " << dataTable.at(0).at(2) << " equals " << dataTable.at(0).at(1) * dataTable.at(0).at(2) << endl; 
+
     cout << "This data set has " << dataTable.at(0).size() - 1 << " features (not including the class attribute), with " << dataTable.size() << " instances." << endl;
 
+    featureSearch(dataTable);
     return 0;
 }
